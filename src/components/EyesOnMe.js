4c31f48f43570d1ebf7ede49cb0ce7e142f1410c
charcodes24 <charcodes24@gmail.com>
2021-06-17 10:47:31 -0700

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+    function focus() {
+        console.log('Good!')
+    }
+
+    function blur() {
+        console.log('Hey! Eyes on me!')
+    }
+    
+    return (
+        <div>
+            <button onFocus={focus} onBlur={blur}>Eyes on Me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
